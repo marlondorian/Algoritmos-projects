114ca599a9f7d8dde1da9435a7966f836e7d0b09
marlondorian <marlondorian35@outlook.com>
2025-08-04 20:51:32 -0600

    First

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,27 @@
 import { useState } from "react"
 import Button from "./components/Button";
+import TodoForm from "./components/TodoForm";
 
 function App() {
 
-  const [count, setCount] = useState(5);
+  const [todos, setTodos] = useState();
+
+  const addTodo = (todo) =>{
+    setTodos([...todos,todo])
+  }
 
   // setCount(10);
 
 
   return (
     <>
-      <div className="counter-container">
-        <div className="counter-card">
-          <div className="counter-display">
-            <span className="counter-number">
-              {count}
-            </span>
-          </div>
-          
-          <div className="counter-controls">
-            <button 
-                className="counter-btn decrement" 
-                onClick={()=>setCount(count-1)}
-                >
-              -
-            </button>
-            <Button label="-" onClick={()=>setCount(count-1)}  />
-            <button 
-              className="counter-btn increment"
-              onClick={()=>setCount(count+1)}
-            >
-              +
-            </button>
-          </div>
-        </div>
+      <div>
+        <div>Hola Mundo</div>
+        <TodoForm addTodo={addTodo}/>
       </div>
     </>
   )
 }
+
 
 export default App
---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+const TodoForm = (addTodo) => {
+
+    const [value, setValue] = useState('')
+
+
+    const handleSubmit = (e) =>{
+        e.preventDefault()
+        if (!value.trim()) return;
+        addTodo(value)
+        setValue('')    
+    }
+
+    return (
+        <form className="todo-form" onSubmit={handleSubmit}>
+            <input 
+                type="text" 
+                className="todo-input"
+                value={value}
+                onChange={e=> setValue(e.target.value)}
+                placeholder="Agrega una tarea...."
+            
+            
+            />
+            <button>Agregar</button>
+        </form>
+    )
+
+
+}
+
+export default TodoForm
